c58f69b8efce98c6061378bd53e63cc240b9d78d
Glafira Bekh <vladbragin@MacBook-Pro--Glashunya.local>
2024-02-21 22:57:08 +0500

    add telegram token as env variable

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const axios = require('axios');
+const dotenv = require('dotenv').config();
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Владик Брагин просто какой-то волшебник, я в шоке, что он делает и что я это сама пишу');    
+})
 
 app.post('/webhook', express.json({type: 'application/json'}), (req, res) => {
     res.status(202).send('Accepted');
 
     const githubEvent = req.headers['x-github-event'];
 
-    axios.post('/test-route', {
+    const TELEGRAM_TOKEN = dotenv.parsed.TELEGRAM_TOKEN;
+
+    axios.post(`https://api.telegram.org/${TELEGRAM_TOKEN}/sendMessage`, {
             chat_id: 343606892,
             text: `Пришло событие ${githubEvent} от репозитория`,
         }
